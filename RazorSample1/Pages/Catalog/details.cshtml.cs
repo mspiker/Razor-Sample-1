9b4238511574136329267516d48be763fcfd6265
Mark Spiker <kb8uvv@gmail.com>
2020-06-19 14:32:38 -0400

    Changes to prototype for catalog.

--- RazorSample1/Pages/Catalog/details.cshtml.cs
+++ RazorSample1/Pages/Catalog/details.cshtml.cs
@@ -4,14 +4,30 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorSample1.Models;
+using RazorSample1.Services;
 
 namespace RazorSample1.Pages.Catalog
 {
     public class detailsModel : PageModel
     {
-        public void OnGet()
-        {
+        private readonly IItemRepository itemRepository;
 
+        public detailsModel(IItemRepository itemRepository)
+        {
+            this.itemRepository = itemRepository;
+        }
+
+        public Item Item { get; private set; }
+
+        public IActionResult OnGet(int id)
+        {
+            Item = itemRepository.GetItem(id);
+            if (Item==null)
+            {
+                return RedirectToPage("/NotFound");
+            }
+            return Page();
         }
     }
 }
